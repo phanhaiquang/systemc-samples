175cff8e6ac85cd0164c8142ee1eacab3092c8dd
quang <phanhaiquang@gmail.com>
2020-04-11 19:37:41 +0700

    dump waveform

--- sample1/main.cpp
+++ sample1/main.cpp
@@ -26,7 +26,19 @@ int sc_main(int argc, char* argv[])
   Monitor1.F(FSig);
   Monitor1.Clk(TestClk);
 
+  sc_trace_file* Tf;
+  Tf = sc_create_vcd_trace_file("traces");
+  // ((vcd_trace_file*)Tf)->sc_set_vcd_time_unit(-9);
+  sc_trace(Tf, ASig  , "A" );
+  sc_trace(Tf, BSig  , "B" );
+  sc_trace(Tf, FSig  , "F" );
+  sc_trace(Tf, DUT.S1, "S1");
+  sc_trace(Tf, DUT.S2, "S2");
+  sc_trace(Tf, DUT.S3, "S3");
+
   sc_start();  // run forever
+  
+  sc_close_vcd_trace_file(Tf);
 
   return 0;
 
--- sample1/mon.h
+++ sample1/mon.h
@@ -1,21 +1,35 @@
+// Download from https://www.doulos.com/knowhow/systemc/tutorial/debugging/
+// Modification:
+//    Add include <iomanip>
+//    Change setw() to std::setw()
+//      https://stackoverflow.com/questions/28971312/iomanip-errors-or-setw-was-not-declared-in-this-scope
 #include "systemc.h"
+#include <iomanip>
+
 SC_MODULE(mon)
 {
-  sc_in<bool> A, B, F, Clk;
+    sc_in<bool> A,B,F;
+    sc_in<bool> Clk;
 
-  void MonDisplay()
+  void monitor()
   {
-    cout << "A\tB\tF\n";
-    cout << "-----------------\n";
-    while(1) {
-      cout << A.read() << "\t" << B.read() << "\t" << F.read() << "\n";
-      wait();
+    cout << std::setw(10) << "Time";
+    cout << std::setw(2) << "A" ;
+    cout << std::setw(2) << "B";
+    cout << std::setw(2) << "F" << endl;
+    while (true)
+    {
+      cout << std::setw(10) << sc_time_stamp();
+      cout << std::setw(2) << A.read();
+      cout << std::setw(2) << B.read();
+      cout << std::setw(2) << F.read() << endl;
+      wait();    // wait for 1 clock cycle
     }
   }
 
   SC_CTOR(mon)
   {
-    SC_THREAD(MonDisplay);
+    SC_THREAD(monitor);
     sensitive << Clk.pos();
   }
 };
